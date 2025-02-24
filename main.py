bd8e68a399cf563478d58419198eecb6998716cf
Moyaz79 <yasinabdu77@outlook.com>
2025-02-24 19:36:03 +0000

    added word_count fn

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ def get_book_text(path_to_file):
 
 def main():
     file_content = get_book_text("books/frankenstein.txt")
-    print(file_content)
+    file_word_count = word_count(file_content)
+    print(f"{file_word_count} words found in the document")
+
+def word_count(s):
+    return len(s.split())
+    
+
+
 
 
 main()
